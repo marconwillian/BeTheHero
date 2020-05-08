c17cdcdaa20592945bd8c1049d79cdd343522286
Marcon Willian <marconwillian@protonmail.com>
2020-05-08 10:52:34 -0400

    frontend - 0.1.9 > and api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://3333-f55a70ad-61ef-4ffd-b728-4404464581a9.ws-us02.gitpod.io/'
+    baseURL: 'https://bethehero-server.herokuapp.com/'
 });
 
 export default api;
