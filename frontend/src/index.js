44c9c2efff6ce7ba7cfcb3292ebf1e7683b24311
Marcon Willian <marconwillian@protonmail.com>
2020-12-31 00:05:05 -0300

    chore: Add sentry config

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+    dsn: process.env.SENTRY_DNS,
+    autoSessionTracking: true,
+    integrations: [
+        new Integrations.BrowserTracing(),
+    ],
+    tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
