2cc327e39941f1c6cb7410c551aac404c84b0cfc
Marcon Willian <marconwillian@protonmail.com>
2020-12-31 08:43:16 -0300

    chore: Update service of develop

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://server-bethehero.marconwillian.dev/'
+    baseURL: 'https://server-bethehero.marconwillian.dev:8080'
 });
 
 export default api;
