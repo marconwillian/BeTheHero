c75a2d40f076cdea98a5c61e20fd2d76056679f3
Marcon Willian <marconwillian@protonmail.com>
2020-12-31 12:56:16 -0300

    chore: Update base of knex

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,7 +1,7 @@
 const knex = require('knex');
 const configuration = require('../config/knexfile');
 
-const config = configuration[process.env.NODE_ENV];
+const config = configuration[process.env.NODE_ENV || 'development'];
 
 const connection = knex(config);
 
